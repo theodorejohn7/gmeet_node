d584a79d9b0c8ea04e858677167010d243c51b93
theodorejohn7 <theodore.john@10decoders.in>
2024-05-23 18:30:37 +0530

    setheader

--- index.js
+++ index.js
@@ -2,9 +2,17 @@
 
 const express = require('express');
 const app = express();
-app.use(express.json());
+// app.use(express.json());
 
 let participants = [123];
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://your-allowed-origin.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.post("/participants", (req, res) => {
   participants = req.body.participants;

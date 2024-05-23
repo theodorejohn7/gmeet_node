b3a5ab49e054cef1ff151fb2d9020d8c9cba14dd
theodorejohn7 <theodore.john@10decoders.in>
2024-05-23 20:16:27 +0530

    url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let participants = [123];
 const allowedOrigins = [
   'https://meet.google.com',
   'https://hz65v3.csb.app',
-  'https://gmeet-fe.vercel.app/'
+  'https://gmeet-fe.vercel.app'
 ];
 
 app.use((req, res, next) => {

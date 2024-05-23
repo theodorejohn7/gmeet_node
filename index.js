5f47a445cec251984ad5ef5f6ff741aac104fb32
theodorejohn7 <theodore.john@10decoders.in>
2024-05-23 21:22:58 +0530

    graphql

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+// server.js
 const express = require('express');
-const app = express();
-app.use(express.json());
+const { ApolloServer, gql } = require('apollo-server-express');
+const { createServer } = require('http');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { useServer } = require('graphql-ws/lib/use/ws');
+const { WebSocketServer } = require('ws');
 
-let participants = [123];
+let participants = [];
 
 const allowedOrigins = [
   'https://meet.google.com',
   'https://hz65v3.csb.app',
   'https://gmeet-fe.vercel.app'
 ];
+
+const app = express();
+app.use(express.json());
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
@@ -20,17 +27,66 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/participants", (req, res) => {
-  participants = req.body.participants;
-  console.log("Received participants:", participants); // Debugging log
-  res.sendStatus(200);
+// GraphQL schema
+const typeDefs = gql`
+  type Query {
+    participants: [String]
+  }
+
+  type Mutation {
+    updateParticipants(participants: [String]!): [String]
+  }
+
+  type Subscription {
+    participantsUpdated: [String]
+  }
+`;
+
+// PubSub for subscriptions
+const { PubSub } = require('graphql-subscriptions');
+const pubsub = new PubSub();
+
+const resolvers = {
+  Query: {
+    participants: () => participants,
+  },
+  Mutation: {
+    updateParticipants: (_, { participants: newParticipants }) => {
+      participants = newParticipants;
+      pubsub.publish('PARTICIPANTS_UPDATED', { participantsUpdated: participants });
+      return participants;
+    },
+  },
+  Subscription: {
+    participantsUpdated: {
+      subscribe: () => pubsub.asyncIterator(['PARTICIPANTS_UPDATED']),
+    },
+  },
+};
+
+// Create GraphQL schema
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Create Apollo server
+const server = new ApolloServer({ schema });
+await server.start();
+
+// Apply middleware to express app
+server.applyMiddleware({ app, path: '/graphql' });
+
+// Create HTTP server
+const httpServer = createServer(app);
+
+// Create WebSocket server
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
 });
 
-app.get("/participants", (req, res) => {
-  console.log("GET /participants called");
-  res.json(participants);
-});
+// Use WebSocket server
+useServer({ schema }, wsServer);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+// Start the server
+httpServer.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
